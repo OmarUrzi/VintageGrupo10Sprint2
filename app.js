f3e7393a1be296275a3bf0a2953cc818d361cefd
lehuel97 <lehuelgreco@gmail.com>
2021-04-12 20:11:18 -0300

    agregue carrito greco

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Servidor funcionando');
 });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
 });
 
-app.get('/register', (req,res)=>{
+app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/micarrito', (req, res) => {
+    res.sendFile(__dirname + '/views/micarrito.html');
+});
+app.get('/productDetail', (req, res) => {
+    res.sendFile(__dirname + '/views/productdetail.html');
 });
